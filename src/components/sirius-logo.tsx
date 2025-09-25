ba611767a72704d85950372a87283282b5ea1876
Kashir <askkashir@gmail.com>
2025-09-25 11:25:46 +0000

    still not this logo

--- src/components/sirius-logo.tsx
+++ src/components/sirius-logo.tsx
@@ -5,9 +5,24 @@ export const SiriusLogo = ({ className }: { className?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 256 256"
-    className={cn("text-foreground", className)}
+    className={cn("h-6 w-6", className)}
     style={{ fill: "currentColor" }}
   >
-    <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216ZM188,84a12,12,0,1,1-12-12A12,12,0,0,1,188,84Z" />
+    <g>
+      <path
+        d="M128 36a92 92 0 1 0 0 184 92 92 0 0 0 0-184Zm0 168a76 76 0 1 1 0-152 76 76 0 0 1 0 152Z"
+        className="text-foreground"
+      />
+      
+      <path
+        d="M112 121 82 91l-46 46 30 30-30 30 46 46 30-30 30 30 46-46-30-30 30-30-46-46-30 30Z"
+        style={{ fill: "#ef4444" }}
+      />
+
+      <path
+        d="M157.4 69.5 128 112h32l-29.4 46.5L160 186h-32l32 40.5 54-54-32.5-49.5 32.5-49.5-54-54Z"
+        className="text-foreground"
+      />
+    </g>
   </svg>
 );
